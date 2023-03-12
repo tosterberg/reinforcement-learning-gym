1a35b1baedbd54e779240ce07b9ab5069a604199
Tyler Osterberg <osterberg614@gmail.com>
2023-03-12 12:15:52 -0700

    [fix] lab writeup checkpoint 3

--- reinforce/experiments/one_armed_bandit.py
+++ reinforce/experiments/one_armed_bandit.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from ..scenarios.bandit import ProvidedBandit
+from reinforce.scenarios.bandit import ProvidedBandit
 
 sns.set()
 """
